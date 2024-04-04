2a3b102037e225f6dae60069b1d5f0beef97d65d
jaye <74174900+shiopao@users.noreply.github.com>
2024-04-05 01:24:36 +0800

    fix: visual indicators for share and pushing to user table

--- src/app/backend/components/utilities/ToggleShare.tsx
+++ src/app/backend/components/utilities/ToggleShare.tsx
@@ -11,8 +11,8 @@ interface Props {
 }
 
 const ToggleShare: React.FC<Props> = ({ post }) => {  
-  const { user } = useGlobalContext();
-  const [isShared, setIsShared] = useState(post?.shares?.includes(user.uuid));
+  const { user, setUser } = useGlobalContext();
+  const [shared, setShared] = useState(post?.shares?.includes(user.uuid));
 
   // TODO - Miguel: Implement notification on share once share functionality is implemented
 
@@ -22,26 +22,60 @@ const ToggleShare: React.FC<Props> = ({ post }) => {
     if (error) throw error;
   };
 
+  const saveUserShares = async () => {
+    const { data, error } = await Supabase.from('profiles').update({ shares: user.shares }).eq('uuid', user.uuid);
+
+    if (error) throw error;
+  };
+
   const handleShare = () => {
     if (user.uuid === '') return;
     
-    if (!isShared) {
+    if (!shared) {
       post?.shares?.push(user.uuid);
+      user.shares?.push(post.id);
       savePostShares();
-      setIsShared(true);
+      saveUserShares();
+      setUser(
+        new UserClass({
+          ...user,
+          shares: user.shares,
+        })
+      );
+      setShared(true);
     } else {
       post?.shares?.splice(post?.shares?.indexOf(user.uuid), 1);
+      user.shares?.splice(user.shares?.indexOf(post.id), 1);
       savePostShares();
-      setIsShared(false);
+      saveUserShares();
+      setUser(
+        new UserClass({
+          ...user,
+          shares: user.shares,
+        })
+      );
+      setShared(false);
     }
   };
 
   const shareCount = (post?.shares?.length || 0)
 
   return (
-    <div className={`interaction-row  ${false ? 'bg-violet-200 hover:bg-violet-300' : 'hover:bg-gray-200 '}`}>
-      <Repeat2 className="" size={12} strokeWidth={3} onClick={handleShare} />
-      <h6 className=" font-normal text-xs">{shareCount}</h6>
+    <div 
+      className={`interaction-row  ${shared ? 'bg-violet-200 hover:bg-violet-300' : 'hover:bg-gray-200 '}`} 
+      onClick={handleShare}
+    >
+      {shared? (
+        <>
+          <Repeat2 className="text-[#6157ff]" size={12} strokeWidth={3} />
+          <h6 className="text-[#6157ff] font-normal text-xs" data-testid="count1">{shareCount}</h6>
+        </>
+      ) : (
+        <>
+          <Repeat2 className="" size={12} strokeWidth={3} />
+          <h6 className="font-normal text-xs" data-testid="count2">{shareCount}</h6>
+        </>
+      )}
     </div>
   );
 };
